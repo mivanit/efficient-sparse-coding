bc6a795635d4fc7fa2ee3d45b38d18f4cf64d23d
Michael I <mivanit@hotmail.com>
2020-04-13 11:43:20 -0400

    misc

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ norm_F = lambda x : np.linalg.norm(x, 'fro')
 phi = lambda x : norm_1(x)
 
 
+# float comparison threshold
+THRESH = 1e-7
+
+def is_zero(x):
+	return abs(x) < THRESH
+
+
 
 class SparseCode(object):
 
@@ -63,10 +70,10 @@ class SparseCode(object):
 			
 			return constraint
 		
-		# TODO: S_guess
+		# TODO: S_guess according to the given distribution
 		S_guess = np.zeros((self.k * m))
 
-		sopt.minimize(
+		return sopt.minimize(
 			fun = mini_me,
 			x0 = S_guess,
 			constraints = [ 
@@ -136,10 +143,24 @@ def feature_sign_search(A, y, gamma):
 	def deriv_yAx(i, x_i = 0.0):
 		r'''
 		returns the derivative
-		\frac{\partial || y - Ax ||^2 }{ \partial x_i }
+			\frac{\partial || y - Ax ||^2 }{ \partial x_i }
 		at x_i = 0 by default
+		note this is equalt to
+			-2 * \sum_{j \in \N_m} 
+			[ y_j - \sum_{k \in \N_p} A_{j,k} x_k ] 
+			* [ A_{j,i} ]
+		see paper_notes.md
 		'''
+		x_cpy = np.copy(x)
+		x_cpy[i] = x_i
 
+		return (-2) * sum(
+			A[j,i] * ( y[j] - sum(
+				A[j,k] * x_cpy[k]
+				for k in range(dim_p)
+			) )
+			for j in range(dim_m)
+		)
 
 
 	## theta = lambda i: (x[i] > 0) - (x[i] < 0)
@@ -148,9 +169,9 @@ def feature_sign_search(A, y, gamma):
 
 	active_set = set()
 
-	satCond_a, satCond_b = False, False
+	opmCond_a, opmCond_b = False, False
 	
-	while not satCond_b:
+	while not opmCond_b:
 
 		# * 2: from zero coefficients of x, select i such that
 		# 		y - A x is changing most rapidly with respect to x_i
@@ -166,27 +187,64 @@ def feature_sign_search(A, y, gamma):
 		i_sel = np.argmax(np.absolute(selector_arr))
 
 		# acivate x_i if it locally improves objective
-		i_deriv = deriv_yAx(i)
+		i_deriv = deriv_yAx(i_sel)
 		if abs(i_deriv) > gamma:
 			theta[i_sel] = (-1) * sign(i_deriv)
 			active_set.add(i_sel)
 
 		active_list = sorted(list(active_set))
 		
-		while not satCond_a:
+		while not opmCond_a:
 
 			# * 3: feature-sign step
+
+			# A_hat is submatrix of A containing only columns corresponding to active set
+			# REVIEW: not sure if this line will work
 			A_hat = A[:,active_list]
 
+			# TODO: whats a clean way to handle x_hat indecies here?
 
-			# TODO
+			# compute solution to unconstrained QP:
+			# minimize_{x_hat} || y - A_hat @ x_hat ||^2 + gamma * theta_hat.T @ x_hat
 
-			# check optimality condition a
+			# TODO: do we /really/ need to compute matrix inverse? can we minimize more efficiently?
+			theta_hat = [sign(x[a]) for a in active_list]
+
+			x_hat_new = np.linalg.inv(A_hat.T @ A_hat) @ (A_hat.T @ y - gamma * theta_hat / 2)
+
+			# perform a discrete line search on segment x_hat to x_hat_new
+
+			# TODO: this whole bit
+
+
+			# * 4: check optimality condition a
+			
+			# set of j where x_j != 0
+			set_j_not0 = {
+				j for j in range(dim_p)
+				if not is_zero( x[j] )
+			}
+
+			opmCond_a = all( 
+				is_zero( deriv_yAx(j) + gamma * sign(x[j]) )
+				for j in set_j_not0
+			)
 
 
 
-		# check optimality condition b
+		# * 4: check optimality condition b
+		# set of j where x_j == 0
+		set_j_is0 = {
+			j for j in range(dim_p)
+			if is_zero( x[j] )
+		}
 
+		opmCond_b = all( 
+			abs(deriv_yAx(j)) <= gamma
+			for j in set_j_is0
+		)
+
+	return x
 
 
 

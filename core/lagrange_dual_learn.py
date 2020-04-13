951cd347d757631528aac05a105587bd75921475
VigJ <vigneshj@umich.edu>
2020-04-13 14:46:39 -0400

    added the filler dual function definition

--- core/lagrange_dual_learn.py
+++ core/lagrange_dual_learn.py
@@ -3,6 +3,9 @@ import numpy as np
 from util import *
 
 def lagrange_dual_learn(solvr_obj, X, c_const):
-	B = solvr_obj.B
-	S = solvr_obj.S
-	n = solvr_obj.n
+    B = solvr_obj.B
+    S = solvr_obj.S
+    n = solvr_obj.n
+    
+    #FILLER, not permanent
+    return (X @ np.linalg.inv(S)).T
